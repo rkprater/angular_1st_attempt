faba34482a5af3c8bd88395fd256af545457eea0
rkprater <rkprater@icloud.com>
2015-03-01 22:51:16 -0500

    added app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+(function () {
+  "use strict";
+
+  angular.module('AmazonApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController as mainCtrl'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'MainController as mainCtrl'
+      })
+      .when('/new', {
+        templateUrl: 'views/new.html',
+        controller: 'MainController as mainCtrl'
+      })
+      .when('/products', {
+        templateUrl: 'views/product.html',
+        controller: 'MainController as mainCtrl'
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html',
+        controller: 'MainController as mainCtrl'
+      })
+      .when('/notfound', {
+        templateUrl: 'views/notfound.html'
+      })
+      .otherwise({
+        redirectTo: '/notfound'
+      });
+  });
+
+})();
